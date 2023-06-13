bccc6b47a77884fb1bd564abdaecf5ecd6275724
layconjohn <layconjohn@gmail.com>
2023-06-13 20:38:23 -0300

    feat(pokemon): add include and select images

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -8,28 +8,41 @@ import { Prisma } from '@prisma/client';
 export class PokemonService {
   constructor(private readonly prisma: PrismaService) {}
 
+  private readonly _include = {
+    images: {
+      select: {
+        url: true,
+      }
+    }
+  };
+
   create(createPokemonDto: CreatePokemonDto) {
     return this.prisma.pokemon.create({
       data: {
         ...createPokemonDto
-      }
+      },
+      include: this._include
     });
   }
 
   findAll() {
-    return this.prisma.pokemon.findMany({});
+    return this.prisma.pokemon.findMany({
+      include: this._include
+    });
   }
 
   findOne(id: number) {
     return this.prisma.pokemon.findUnique({
-      where: {id}
+      where: {id},
+      include: this._include
     });
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
     return this.prisma.pokemon.update({
       where: {id},
-      data: {...updatePokemonDto}
+      data: {...updatePokemonDto},
+      include: this._include
     });
   }
 
